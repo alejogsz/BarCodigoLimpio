8c8a66c708fa6e993ee4bacc7f896fca1320241d
alejogsz <140736259+alejogsz@users.noreply.github.com>
2024-11-07 11:28:29 -0500

    Delete codigo limpioi directory

--- codigo limpioi/Producto.py
+++
@@ -1,9 +0,0 @@
-
-
-class Producto:
-    def __init__(self, nombre:str , precio:int)->None:
-        self.nombre:str = nombre
-        self.precio:int = precio
-
-    def obtener_precio(self)->int:
-        return self.precio
